f3c4a8b2a8b62248140f3b7d050cbc7c3155bc8d
shreyamkela <shreyamkela@gmail.com>
2019-04-30 22:47:39 -0700

    final commit - refactoring route for better modularity

--- backend/app.js
+++ backend/app.js
@@ -26,37 +26,8 @@ app.use(function(req, res, next) {
 
 // Routing
 var apiGraph = require("./routes/apiGraph");
-
 // Route configurations
 app.use("/api/graph", apiGraph);
-
-app.post("/", (req, res) => {
-  let query = req.body.query;
-  console.log(query);
-  let value = null;
-  try {
-    // if expression is invalid then eval will throw error and would send 500 error code to frontend, therefore we need a try catch to tackle invalid expressions
-    // 2 types of errors can be caught - 1) If the expression is invalid, eval doesnt work and crashes, 2) if there is math error i.e divide by zero, eval outputs infinity
-    value = eval(query); // eval function can evaluate a valid mathematical expression - https://www.geeksforgeeks.org/javascript-eval-function/
-    // FIXME Round off value to 2-3 decimal places if decimal exists in the value
-    console.log("value" + value);
-    console.log();
-    if (value == Infinity) {
-      throw e;
-    }
-    res.status(200).send({ value: value }); // Send value and success status to 3000
-  } catch (e) {
-    // let value = "INVALID INPUT";
-    // res.statusMessage("invalid");
-    if (value == Infinity) {
-      res.status(400).send("INFINITY");
-    } else {
-      console.log("INVALID EXPRESSION");
-
-      res.status(400).send("INVALID EXPRESSION"); // Send "Invalid input" and Bad request error 400 status to 3000
-    }
-  }
-});
 
 app.listen(port, () => {
   console.log(`Express Server is up on port ${port}`);
